1981c0a5d760c469c568f1211f8dda8d48b21f3c
Manoj Garje <manojgarje123@gmail.com>
2026-02-17 17:06:55 +0530

    Fixes routes add to the correct file

--- src/modules/auth/auth.service.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('./auth.controller');
-
-
-router.post("/regsiter", authController.register);
-router.post("/login", authController.login);
-
-module.exports = router;
