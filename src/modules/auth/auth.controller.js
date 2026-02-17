07aac012fbb6d1fb2c8c3602cbf2d87a50bbf50d
Manoj Garje <manojgarje123@gmail.com>
2026-02-17 17:27:33 +0530

    Switch to ES module

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,29 @@
+import authService from './auth.service';
+
+export const register = async (req, res, next) => {
+    try{
+        const data = await authService.register(req.body);
+        res.status(201).json(data);
+
+    }catch(error){
+        next(error);
+    }
+}
+
+export const login = async (req, res, next) => {
+    try{
+        const data = await authService.login(req.body);
+        res.status(200).json(data);
+    }catch(error){
+        next(error)
+    }   
+}
+
+export const refresh = async (req, res, next) => {
+  try {
+    const data = await authService.refresh(req.body.refreshToken);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
