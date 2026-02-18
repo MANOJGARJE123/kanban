96b8a126a7423932062bee2b5d936fd4f3e49cdf
Manoj Garje <manojgarje123@gmail.com>
2026-02-18 16:11:57 +0530

    Adds login

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -2,9 +2,10 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import pool from '../config/db.js';
 
+
 export const register = async (data) => {
     const existingUser = await pool.query(
-        'SELECT * FROM users WHERE email = $1', [data.email]
+        'select * from users where email = $1', [data.email]
     );
 
     if (existingUser.rows.length > 0) {
@@ -14,15 +15,48 @@ export const register = async (data) => {
     const hashedPassword = await bcrypt.hash(data.password, 10);
 
     const result = await pool.query (
-        'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id',
+        'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id',
         [data.email, hashedPassword]
     )
+
+    return {
+        id: result.rows[0].id,
+        message: "User registered successfully"
+    };
 };
 
 export const login = async (data) => {
+    const userResult = await pool.query(
+        'select id, email, password_hash from users where email = $1', [data.email]
+    );
 
-};
+    if (userResult.rows.length === 0) {
+        throw new Error('Invalid email or password');
+    }
+    const user = userResult.rows[0];
 
-export const refresh = async (refreshToken) => {
- 
+    const isPasswordValid = await bcrypt.compare(data.password, user.password_hash);
+    if (!isPasswordValid) {
+        throw new Error('Invalid email or password');
+    } 
+    
+    const JWT_SECRET = process.env.JWT_SECRET;
+
+    if (!JWT_SECRET) {
+        throw new Error("JWT_SECRET not defined");
+    }
+
+    const token = jwt.sign(
+        { id: user.id, email: user.email },
+        JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+
+    return {
+        token,
+        user: {
+            id: user.id,
+            email: user.email
+        }
+    };
 };
---
+++ src/modules/organization/organization.route.js
@@ -0,0 +1,1 @@
+import { default as organizationController } from './organization.controller.js';
