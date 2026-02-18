5123a12aefe597ebf7e0c21212d9e5a6b9c2f482
Manoj Garje <manojgarje123@gmail.com>
2026-02-18 10:08:22 +0530

    Adds Register user

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,8 +1,22 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import pool from '../config/db.js';
 
 export const register = async (data) => {
- 
+    const existingUser = await pool.query(
+        'SELECT * FROM users WHERE email = $1', [data.email]
+    );
+
+    if (existingUser.rows.length > 0) {
+        throw new Error('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const result = await pool.query (
+        'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id',
+        [data.email, hashedPassword]
+    )
 };
 
 export const login = async (data) => {
