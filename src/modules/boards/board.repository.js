608920f1a8afb2ee151558317e8658c2872aade5
Manoj Garje <manojgarje123@gmail.com>
2026-02-19 15:42:16 +0530

    Fixes route to the correct route file

---
+++ src/modules/boards/board.repository.js
@@ -0,0 +1,25 @@
+import pool from '../../config/db.js';
+
+export const createBoard = async (data) => {
+    const result = await pool.query(
+        'INSERT INTO boards (name, organization_id) values ($1, $2) RETURNING id, name, organization_id',
+        [data.name, data.organization_id]
+    )
+    return result.rows[0];
+}
+
+export const getAllBoardsOrganizationId = async (organizationId) => {
+    const result = await pool.query(
+        'SELECT id, name, organization_id FROM boards WHERE organization_id = $1',
+        [organizationId]
+    )
+    return result.rows;
+}
+
+export const getBoardById = async (id) => {
+    const result = await pool.query (
+        'SELECT id, name, organization_id FROM boards WHERE id = $1',
+        [id]
+    )
+    return result.rows[0];
+}
--- src/modules/boards/board.service.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as boardController from './board.controller.js';
-
-const router = express.Router();
-
-router.post('/', boardController.createBoard);
-router.get('/:id', boardController.getBoardById);
-
-export default router;  
