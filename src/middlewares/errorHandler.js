372aaddd87a970c5d83d478ba36a68460ff6fe44
Manoj Garje <manojgarje123@gmail.com>
2026-02-20 13:41:46 +0530

    Changes error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
     console.error('Error:', err.message)
     console.error(err.stack);
 
-    const statusCode = err.statusCode || err.status || 500;
+    const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
     res.status(statusCode).json({
