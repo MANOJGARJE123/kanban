a5ecafd8601e9ab7239a9c2dc973be1582ee8e53
Manoj Garje <manojgarje123@gmail.com>
2026-02-23 16:26:28 +0530

    Adds Message to proper output

--- src/modules/tasks/task.controller.js
+++ src/modules/tasks/task.controller.js
@@ -3,7 +3,7 @@ import * as taskServices from './task.service.js';
 export const createTask = async (req, res, next) => {
     try {
         const data = await taskServices.createTask(req.body);
-        res.status(201).json({ success: true, data });
+        res.status(201).json({ success: true, message: "Task created successfully", data });
     } catch (error) {
         next(error);
     }
@@ -13,7 +13,7 @@ export const getTasks = async (req, res, next) => {
     try {
         const { boardId, columnId } = req.query;
         const data = await taskServices.getTasks(boardId, columnId);
-        res.status(200).json({ success: true, data });
+        res.status(200).json({ success: true, message: "Tasks fetched successfully", data });
     } catch (error) {
         next(error);
     }
@@ -22,7 +22,7 @@ export const getTasks = async (req, res, next) => {
 export const updateTask = async (req, res, next) => {
     try {
         const data = await taskServices.updateTask(req.params.id, req.body);
-        res.status(200).json({ success: true, data });
+        res.status(200).json({ success: true, message: "Task updated successfully", data });
     } catch (error) {
         next(error);
     }
@@ -32,7 +32,7 @@ export const moveTask = async (req, res, next) => {
     try {
         const { taskId, column_id, position } = req.body;
         const data = await taskServices.moveTask(taskId, column_id, position);
-        res.status(200).json({ success: true, data });
+        res.status(200).json({ success: true,  message: "Task moved successfully", data });
     } catch (error) {
         next(error);
     }
@@ -42,7 +42,7 @@ export const deleteTask = async (req, res, next) => {
     try {
         const taskId = req.params.id;
         const data = await taskServices.deleteTask(taskId);
-        res.status(200).json({ success: true, data });
+        res.status(200).json({ success: true,  message: "Task deleted successfully",data });
     } catch (error) {
         next(error);
     }
